d1d9c616f05f2e1bebfc3bbecd69afa25ee90729
VvsJaemin <djd453216@nate.com>
2023-01-24 17:18:34 +0900

    useState 및 양방향 바인딩

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import ExpenseItem from "./components/Expenses/ExpenseItem";
 import Expenses from "./components/Expenses/Expenses";
+import NewExpense from "./components/NewExpense/NewExpense";
+import ExpenseForm from "./components/NewExpense/ExpenseForm";
 function App() {
     const expenses = [
         {
@@ -24,7 +26,7 @@ function App() {
     ];
   return (
     <div>
-        <h2>Let's get started</h2>
+        <NewExpense />
         <Expenses items={expenses}/>
     </div>
   );
